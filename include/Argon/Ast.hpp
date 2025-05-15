e99403cbe53574fa455dbd0847b7bdb19d3bef70
DragonMan14 <andyphuhuynh1@gmail.com>
2025-05-15 15:32:49 -0400

    Refactor Ast.tpp and Context.tpp into their respective hpp files

--- include/Argon/Ast.hpp
+++ include/Argon/Ast.hpp
@@ -4,75 +4,70 @@
 #include <string>
 #include <vector>
 
+#include "Scanner.hpp"
 #include "Error.hpp"
 
 namespace Argon {
-    class IOption;
-    class Parser;
-    class OptionAst;
-    class OptionGroupAst;
-    class StatementAst;
     class Context;
-    class Token;
-    
+
     struct Value {
         std::string value;
-        int pos;
+        int pos = 0;
     };
-    
+
     class Ast {
     public:
-        inline virtual void analyze(ErrorGroup& errorGroup, Context& context) = 0;
+        virtual void analyze(ErrorGroup& errorGroup, Context& context) = 0;
     protected:
-        inline virtual ~Ast() = default;
+        virtual ~Ast() = default;
     };
 
     class OptionBaseAst : public Ast {
     public:
         Value flag;
-        
-        inline ~OptionBaseAst() override = default;
+
+        ~OptionBaseAst() override = default;
     protected:
-        inline explicit OptionBaseAst(const Token& flagToken);
+        explicit OptionBaseAst(const Token& flagToken);
     };
-    
+
     class OptionAst : public OptionBaseAst {
     public:
         Value value;
-        
-        inline OptionAst(const Token& flagToken, const Token& valueToken);
-        inline ~OptionAst() override = default;
 
-        inline void analyze(ErrorGroup& errorGroup, Context& context) override;
+        OptionAst(const Token& flagToken, const Token& valueToken);
+        ~OptionAst() override = default;
+
+        void analyze(ErrorGroup& errorGroup, Context& context) override;
     };
 
     class MultiOptionAst : public OptionBaseAst {
         std::vector<Value> m_values;
 
     public:
-        inline explicit MultiOptionAst(const Token& flagToken);
-        inline ~MultiOptionAst() override = default;
+        explicit MultiOptionAst(const Token& flagToken);
+        ~MultiOptionAst() override = default;
 
-        inline void addValue(const Token& value);
-        inline void analyze(ErrorGroup& errorGroup, Context& context) override;
+        void addValue(const Token& value);
+        void analyze(ErrorGroup& errorGroup, Context& context) override;
     };
-    
+
     class OptionGroupAst : public OptionBaseAst {
     public:
         int endPos = -1;
 
-        inline explicit OptionGroupAst(const Token& flagToken);
-        
-        inline OptionGroupAst(const OptionGroupAst&) = delete;
-        inline OptionGroupAst& operator=(const OptionGroupAst&) = delete;
-        
-        inline OptionGroupAst(OptionGroupAst&&) noexcept = default;
-        inline OptionGroupAst& operator=(OptionGroupAst&&) noexcept = default;
-        
-        inline ~OptionGroupAst() override = default;
-        
-        inline void addOption(std::unique_ptr<OptionBaseAst> option);
-        inline void analyze(ErrorGroup& errorGroup, Context& context) override;
+        explicit OptionGroupAst(const Token& flagToken);
+
+        OptionGroupAst(const OptionGroupAst&) = delete;
+        OptionGroupAst& operator=(const OptionGroupAst&) = delete;
+
+        OptionGroupAst(OptionGroupAst&&) noexcept = default;
+        OptionGroupAst& operator=(OptionGroupAst&&) noexcept = default;
+
+        ~OptionGroupAst() override = default;
+
+        void addOption(std::unique_ptr<OptionBaseAst> option);
+        void analyze(ErrorGroup& errorGroup, Context& context) override;
     private:
         std::vector<std::unique_ptr<OptionBaseAst>> m_options;
     };
@@ -80,21 +75,137 @@ namespace Argon {
     class StatementAst : public Ast {
     public:
 
-        inline StatementAst() = default;
-        
-        inline StatementAst(const StatementAst&) = delete;
-        inline StatementAst& operator=(const StatementAst&) = delete;
-        
-        inline StatementAst(StatementAst&&) noexcept = default;
-        inline StatementAst& operator=(StatementAst&&) noexcept = default;
-        
-        inline ~StatementAst() override = default;
-        
-        inline void addOption(std::unique_ptr<OptionBaseAst> option);
-        inline void analyze(ErrorGroup& errorGroup, Context& context) override;
+        StatementAst() = default;
+
+        StatementAst(const StatementAst&) = delete;
+        StatementAst& operator=(const StatementAst&) = delete;
+
+        StatementAst(StatementAst&&) noexcept = default;
+        StatementAst& operator=(StatementAst&&) noexcept = default;
+
+        ~StatementAst() override = default;
+
+        void addOption(std::unique_ptr<OptionBaseAst> option);
+        void analyze(ErrorGroup& errorGroup, Context& context) override;
     private:
         std::vector<std::unique_ptr<OptionBaseAst>> m_options;
     };
 }
 
-#include "Ast.tpp"
+// --------------------------------------------- Implementations -------------------------------------------------------
+
+#include <utility>
+
+#include "Argon/Ast.hpp"
+
+#include "Context.hpp"
+#include "Option.hpp"
+#include "MultiOption.hpp"
+
+inline Argon::OptionBaseAst::OptionBaseAst(const Token& flagToken) {
+    flag = { .value = flagToken.image, .pos = flagToken.position };
+}
+
+// OptionAst
+
+inline Argon::OptionAst::OptionAst(const Token& flagToken, const Token& valueToken)
+    : OptionBaseAst(flagToken) {
+    value = { .value = valueToken.image, .pos = valueToken.position };
+}
+
+inline void Argon::OptionAst::analyze(ErrorGroup& errorGroup, Context& context) {
+    IOption *iOption = context.getOption(flag.value);
+    if (!iOption) {
+        errorGroup.addErrorMessage(std::format("Unknown option: '{}'", flag.value), flag.pos);
+        return;
+    }
+
+    OptionBase *optionBase = dynamic_cast<OptionBase*>(iOption);
+    if (!optionBase || !dynamic_cast<IsSingleOption*>(iOption)) {
+        errorGroup.addErrorMessage(std::format("Flag '{}' is not an option", flag.value), flag.pos);
+        return;
+    }
+
+    optionBase->set_value(flag.value, value.value);
+    if (iOption->has_error()) {
+        errorGroup.addErrorMessage(iOption->get_error(), value.pos);
+    }
+}
+
+// MultiOptionAst
+
+inline Argon::MultiOptionAst::MultiOptionAst(const Token& flagToken) : OptionBaseAst(flagToken) {
+
+}
+
+inline void Argon::MultiOptionAst::addValue(const Token& value) {
+    m_values.emplace_back(value.image, value.position);
+}
+
+inline void Argon::MultiOptionAst::analyze(ErrorGroup& errorGroup, Context &context) {
+    IOption *iOption = context.getOption(flag.value);
+    if (!iOption) {
+        errorGroup.addErrorMessage(std::format("Unknown multi-option: '{}'", flag.value), flag.pos);
+        return;
+    }
+
+    OptionBase *optionBase = dynamic_cast<OptionBase*>(iOption);
+    if (!optionBase || !dynamic_cast<IsMultiOption*>(iOption)) {
+        errorGroup.addErrorMessage(std::format("Flag '{}' is not a multi-option", flag.value), flag.pos);
+        return;
+    }
+
+    for (const auto&[value, pos] : m_values) {
+        optionBase->set_value(flag.value, value);
+        if (iOption->has_error()) {
+            errorGroup.addErrorMessage(iOption->get_error(), pos);
+        }
+    }
+}
+
+// OptionGroupAst
+
+inline Argon::OptionGroupAst::OptionGroupAst(const Token& flagToken)
+    : OptionBaseAst(flagToken) {}
+
+inline void Argon::OptionGroupAst::addOption(std::unique_ptr<OptionBaseAst> option) {
+    if (option == nullptr) {
+        return;
+    }
+    m_options.push_back(std::move(option));
+}
+
+inline void Argon::OptionGroupAst::analyze(ErrorGroup& errorGroup, Context& context) {
+    IOption *iOption = context.getOption(flag.value);
+    if (!iOption) {
+        errorGroup.removeErrorGroup(flag.pos);
+        errorGroup.addErrorMessage(std::format("Unknown option group: '{}'", flag.value), flag.pos);
+        return;
+    }
+
+    OptionGroup *optionGroup = dynamic_cast<OptionGroup*>(iOption);
+    if (!optionGroup) {
+        errorGroup.removeErrorGroup(flag.pos);
+        errorGroup.addErrorMessage(std::format("Flag '{}' is not an option group", flag.value), flag.pos);
+        return;
+    }
+
+    for (const auto& option : m_options) {
+        option->analyze(errorGroup, optionGroup->get_context());
+    }
+}
+
+// StatementAst
+
+inline void Argon::StatementAst::addOption(std::unique_ptr<OptionBaseAst> option) {
+    if (option == nullptr) {
+        return;
+    }
+    m_options.push_back(std::move(option));
+}
+
+inline void Argon::StatementAst::analyze(ErrorGroup& errorGroup, Context& context) {
+    for (const auto& option : m_options) {
+        option->analyze(errorGroup, context);
+    }
+}
--- include/Argon/Context.hpp
+++ include/Argon/Context.hpp
@@ -21,18 +21,52 @@ namespace Argon {
 
         template <typename T>
         T *getOptionDynamic(const std::string& flag);
-        
+
         [[nodiscard]] bool containsLocalFlag(const std::string& flag) const;
     };
 }
 
-// Template Implementations
+// --------------------------------------------- Implementations -------------------------------------------------------
 
-namespace Argon {
-    template <typename T>
-    T* Context::getOptionDynamic(const std::string& flag) {
-        return dynamic_cast<T*>(getOption(flag));
+#include "Argon/Context.hpp"
+
+#include <algorithm>
+
+#include "Option.hpp" // NOLINT (misc-unused-include)
+
+inline Argon::Context::Context(const Context& other) {
+    for (const auto& option : other.m_options) {
+        m_options.push_back(option->clone());
     }
+    m_flags = other.m_flags;
 }
 
-#include "Context.tpp"
+inline Argon::Context& Argon::Context::operator=(const Context& other) {
+    m_options.clear();
+    for (const auto& option : other.m_options) {
+        m_options.push_back(option->clone());
+    }
+    m_flags = other.m_flags;
+    return *this;
+}
+
+inline void Argon::Context::addOption(const IOption& option) {
+    m_flags.insert(m_flags.end(), option.get_flags().begin(), option.get_flags().end());
+    m_options.push_back(option.clone());
+}
+
+inline Argon::IOption* Argon::Context::getOption(const std::string& flag) {
+    const auto it = std::ranges::find_if(m_options, [&flag](const auto& option) {
+       return std::ranges::contains(option->get_flags(), flag);
+    });
+    return it == m_options.end() ? nullptr : it->get();
+}
+
+template <typename T>
+T* Argon::Context::getOptionDynamic(const std::string& flag) {
+    return dynamic_cast<T*>(getOption(flag));
+}
+
+inline bool Argon::Context::containsLocalFlag(const std::string& flag) const {
+    return std::ranges::contains(m_flags, flag);
+}
